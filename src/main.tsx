74ac4250bfb33af21b26a85308a8b2bd3821c66f
Tina Johnston <tinajohnston@BCI-SW1P-MAC006.local>
2023-08-16 15:49:07 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { createClient, cacheExchange, fetchExchange, Provider } from 'urql';
+
+const client = createClient({
+  url: import.meta.env.VITE_API_URL || 'http://localhost:4000/graphql',
+  exchanges: [cacheExchange, fetchExchange]
+});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Provider value={client}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
